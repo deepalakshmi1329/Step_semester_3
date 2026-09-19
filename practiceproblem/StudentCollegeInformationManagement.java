78827896f5bbb68ddf2a495ccb66e866a253900a
deepukathir29 <deepukathir29@gmail.com>
2026-09-19 16:51:22 +0530

    week 6 problems

---
+++ practiceproblem/StudentCollegeInformationManagement.java
@@ -0,0 +1,31 @@
+package practiceproblem;
+
+class Student {
+    String name;
+    double attendance;
+
+    static String collegeName =
+            "SRM Institute of Science and Technology";
+
+    static int studentCount = 0;
+
+    Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students: " + studentCount);
+    }
+}
+
+public class StudentCollegeInformationManagement {
+    public static void main(String[] args) {
+        Student s1 = new Student("Ravi", 85.5);
+        Student s2 = new Student("Anitha", 90.0);
+
+        Student.printCollegeInfo();
+    }
+}
